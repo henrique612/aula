8500bc2ffda838bd8b3e766b3d572e8154249d35
Alunos <Alunos@PTALI20227.animaedu.intranet>
2022-10-10 20:55:44 -0300

    ii

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE } = process.env;
 
